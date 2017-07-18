23e9063a40acf41f66b2512f8f3f4411d5baba7d
IlyaFinkelshteyn <ilyaf@appveyor.com>
2017-07-17 20:57:04 -0700

    add class class library

---
+++ classicdotnet-nuget-pack/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classicdotnet_nuget_pack
+{
+    public class Class1
+    {
+    }
+}
